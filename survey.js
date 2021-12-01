44b2ff4829b2f3fe74e4fe257e7da7eeade73454
Michel <Michel_motongi@hotmail.com>
2021-11-30 19:52:54 -0500

    test

---
+++ survey.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const r1 = readline.createInterface({ input, output });
+
+const ansObj = {
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: ""
+};
+
+const r ={
+    0:"What's your name? Nicknames are also acceptable :)",
+    1:"What's an activity you like doing?",
+    2:"What do you listen to while doing that?",
+    3:"Which meal is your favourite (eg: dinner, brunch, etc.)",
+    4:"What's your favourite thing to eat for that meal?",
+    5:"Which sport is your absolute favourite?",
+    6:"What is your superpower? In a few words, tell us what you are amazing at!"
+}
