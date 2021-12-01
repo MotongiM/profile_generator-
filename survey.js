212a210b218d5e58159605292beabbc23548573d
Michel <Michel_motongi@hotmail.com>
2021-11-30 23:13:34 -0500

    Random survey

--- survey.js
+++ survey.js
@@ -1,24 +1,22 @@
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 
-const r1 = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input,output});
 
-const ansObj = {
-    0: "",
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: ""
-};
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+    rl.question("What's an activity you like doing? ", (activity) => {
+      rl.question("What do you listen to while doing that? ", (listen) => {
+        rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (meal) => {
+          rl.question("What's your favourite thing to eat for that meal? ", (food) => {
+            rl.question("Which sport is your absolute favourite? ", (sport) => {
+              rl.question("What is your superpower? ", (superpower) => {
+                console.log(`${name} likes to listen to ${listen} while ${activity},they like to eat ${food} for ${meal}, they like ${sport} over any other sport and their superpower is ${superpower}.`);
+                rl.close();
+              });
+            });
+          });
+        });
+      });
+    });
+});
 
-const r ={
-    0:"What's your name? Nicknames are also acceptable :)",
-    1:"What's an activity you like doing?",
-    2:"What do you listen to while doing that?",
-    3:"Which meal is your favourite (eg: dinner, brunch, etc.)",
-    4:"What's your favourite thing to eat for that meal?",
-    5:"Which sport is your absolute favourite?",
-    6:"What is your superpower? In a few words, tell us what you are amazing at!"
-}
